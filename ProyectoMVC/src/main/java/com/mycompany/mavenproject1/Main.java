e6f31aa0cc3227fd12b25eb75490e829784b76b8
hatzumy <tellezmaleja@gmail.com>
2020-10-16 07:51:29 -0500

    Se llama la vista, el controlador y el modelo de login en la clase principal

--- ProyectoMVC/src/main/java/com/mycompany/mavenproject1/Main.java
+++ ProyectoMVC/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package com.mycompany.mavenproject1;
-
+import Controlador.Login;
+import Modelo.EmpleadoDAO;
+import Vista.JFPrincipal;
 /**
  *
  * @author Mary Alejandra
@@ -13,6 +15,10 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         
-       
+       JFPrincipal vistaL = new JFPrincipal();
+        EmpleadoDAO modeloL = new EmpleadoDAO();
+        Login login= new Login(vistaL, modeloL);
+        vistaL.setVisible(true);
+        vistaL.setLocationRelativeTo(null);
     }
 }
