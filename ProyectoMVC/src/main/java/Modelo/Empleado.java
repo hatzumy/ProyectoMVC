b57ce4d1b161f9683d06d0ae834d4de1a79eed1a
hatzumy <tellezmaleja@gmail.com>
2020-10-16 07:43:08 -0500

    Se añade el encapsulado y el DAO

--- ProyectoMVC/src/main/java/Modelo/Empleado.java
+++ ProyectoMVC/src/main/java/Modelo/Empleado.java
@@ -10,5 +10,47 @@ package Modelo;
  * @author Mary Alejandra
  */
 public class Empleado {
+    String cedula ;
+    String contraseña;
+    String apellidos;
+    String nombres;
     
+    public Empleado(){
+        cedula="";
+        contraseña="";
+        apellidos="";
+        nombres="";
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
 }
--- ProyectoMVC/src/main/java/Modelo/EmpleadoDAO.java
+++ ProyectoMVC/src/main/java/Modelo/EmpleadoDAO.java
@@ -10,5 +10,26 @@ package Modelo;
  * @author Mary Alejandra
  */
 public class EmpleadoDAO {
-    
+    public Empleado verificaUsuario(String cedula, String contraseña){
+        Empleado empleado=null;
+        String cc = "123456789";
+        String password = "12345";
+        String apellidos = "Perez";
+        String nombres = "Pedro";
+        System.out.println(cedula+"    "+ contraseña);
+        try {
+            empleado = new Empleado();
+            if(cedula.equals("12345") && contraseña.equals("12345")){
+                System.out.println("entro");
+               empleado = new Empleado();
+               empleado.setCedula(cc);
+               empleado.setContraseña(password);
+               empleado.setApellidos(apellidos);
+               empleado.setNombres(nombres);
+               return empleado;
+            }
+        } catch (Exception e) {
+        }
+        return empleado;
+    }
 }
