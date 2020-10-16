18ee675bddde7b8c60a029dab0b91ddd927444d8
hatzumy <tellezmaleja@gmail.com>
2020-10-16 07:49:51 -0500

    Se añade los controladores de login y empleado

--- ProyectoMVC/src/main/java/Controlador/CEmpleado.java
+++ ProyectoMVC/src/main/java/Controlador/CEmpleado.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package Controlador;
+import Modelo.Empleado;
+import Modelo.EmpleadoDAO;
+import Vista.JFEmpleado;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  *
  * @author Mary Alejandra
  */
-public class CEmpleado {
+public class CEmpleado implements ActionListener {
+    JFEmpleado vistaEmpleado = new JFEmpleado();
+    EmpleadoDAO modeloEmpleado = new EmpleadoDAO();
+    Empleado  empleado = new Empleado();
+    String cedula, contraseña, privilegio;
+    
+    public CEmpleado(JFEmpleado vistaEmpleado, EmpleadoDAO modeloEmpleado){
+        this.vistaEmpleado=vistaEmpleado;
+        this.modeloEmpleado=modeloEmpleado;
+        this.vistaEmpleado.jBDatos.addActionListener(this);
+    }
+    
+    public void InicializaEmpleado(String cedula, String contraseña){
+        this.cedula = cedula;
+        this.contraseña=contraseña;
+    }
+    
+    public void actionPerformed(ActionEvent e){
+        empleado = modeloEmpleado.verificaUsuario(cedula, contraseña);
+        JOptionPane.showMessageDialog(vistaEmpleado, "DATOS DE USUARIO EMPLEADO \n"+
+                "Nombres: "+empleado.getApellidos()+"\n Apellidos:"+empleado.getNombres()+"\n");
+    }
     
 }
--- ProyectoMVC/src/main/java/Controlador/Login.java
+++ ProyectoMVC/src/main/java/Controlador/Login.java
@@ -4,11 +4,49 @@
  * and open the template in the editor.
  */
 package Controlador;
+import javax.swing.*;
 
+import Modelo.Empleado;
+import Modelo.EmpleadoDAO;
+import Vista.JFEmpleado;
+import Vista.JFPrincipal;
+import java.awt.event.*;
 /**
  *
  * @author Mary Alejandra
  */
-public class Login {
+public class Login implements ActionListener{
+    JFPrincipal vistaLogin = new JFPrincipal();
+    EmpleadoDAO modeloLogin = new EmpleadoDAO();
+    Empleado empleado = new Empleado();
     
+    public Login(JFPrincipal vistaLogin, EmpleadoDAO modeloLogin){
+        this.vistaLogin=vistaLogin;
+        this.modeloLogin = modeloLogin;
+        this.vistaLogin.jBIngresar.addActionListener(this);
+    }
+    public void InicializarLogin(){
+    }
+      
+     public void actionPerformed(ActionEvent e){
+        
+        String cedula= vistaLogin.jTCedula.getText();
+        String contraseña = String.valueOf(vistaLogin.jPContraseña.getPassword());
+        System.out.println(cedula+ "        "+contraseña);
+        empleado = modeloLogin.verificaUsuario(cedula, contraseña);
+         System.out.println("esto es:"+empleado.getCedula());
+        if(empleado==null ||empleado.getCedula().equals("")){
+            JOptionPane.showMessageDialog(vistaLogin, "Empleado con datos ingresados no encontrado.");
+        }else{
+            JOptionPane.showMessageDialog(vistaLogin, "Datos correctos.");
+            JFEmpleado vistaG = new JFEmpleado();
+            vistaG.jLBienvenido.setText("Bienvenido "+empleado.getNombres());
+            CEmpleado CEmpleado = new CEmpleado (vistaG, modeloLogin);
+            CEmpleado.InicializaEmpleado(cedula, contraseña);
+            vistaG.setVisible(true);
+            vistaG.setLocationRelativeTo(null);
+            vistaLogin.setVisible(false);
+            
+        }
+    }
 }
