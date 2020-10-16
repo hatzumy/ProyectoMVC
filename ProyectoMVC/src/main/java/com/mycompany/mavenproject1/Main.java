46e26c96482aa95991f14ae87d63a29cc7526198
hatzumy <tellezmaleja@gmail.com>
2020-10-16 07:41:00 -0500

    Creacion del proyecto y los cascarones

---
+++ ProyectoMVC/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Mary Alejandra
+ */
+public class Main {
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+    }
+}
